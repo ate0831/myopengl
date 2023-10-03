17e4763cd8ba0720ba8009786c26f20c8b738235
atte0831 <ateateatte@gmail.com>
2023-10-03 14:28:48 +0900

    Add Qube drawing

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use c_str_macro::c_str;
 use cgmath::perspective;
 use cgmath::prelude::SquareMatrix;
 use gl::types::{GLfloat,GLsizei,GLsizeiptr};
+use imgui::im_str;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 
@@ -15,10 +16,6 @@ mod vertex;
 use shader::Shader;
 use vertex::Vertex;
 
-//im_str!マクロを使用するために追加
-#[macro_use]
-extern crate imgui;
-
 #[allow(dead_code)]
 type Point3 = cgmath::Point3<f32>;
 #[allow(dead_code)]
@@ -26,10 +23,10 @@ type Vector3 = cgmath::Vector3<f32>;
 #[allow(dead_code)]
 type Matrix4 = cgmath::Matrix4<f32>;
 
-const WINODW_WIDTH :u32 = 640;
+const WINODW_WIDTH :u32 = 900;
 const WINODW_HEIGHT :u32 = 480;
 const FLOAT_NUM :usize = 3;
-const VERTEX_NUM :usize = 3;
+const VERTEX_NUM :usize = 36;
 const BUF_LEN: usize = FLOAT_NUM * VERTEX_NUM;
 
 fn main() {
@@ -69,10 +66,61 @@ fn main() {
     //set buffer
     #[rustfmt::skip]
     let buffer_array:[f32;BUF_LEN] = [
-        -1.0, -1.0, 0.0,
-        1.0, -1.0, 0.0,
+        //1
+        0.0, 0.0, 0.0,
         0.0, 1.0, 0.0,
+        1.0, 1.0, 0.0,
+
+        0.0, 0.0, 0.0,
+        1.0, 1.0, 0.0,
+        1.0, 0.0, 0.0,
+
+        //2
+        0.0, 0.0, 1.0,
+        0.0, 0.0, 0.0,
+        1.0, 0.0, 0.0,
+
+        0.0, 0.0, 1.0,
+        1.0, 0.0, 0.0,
+        1.0, 0.0, 1.0,
+
+        //3
+        0.0, 1.0, 1.0,
+        0.0, 0.0, 1.0,
+        1.0, 0.0, 1.0,
+
+        0.0, 1.0, 1.0,
+        1.0, 0.0, 1.0,
+        1.0, 1.0, 1.0,
+
+        //4
+        0.0, 1.0, 0.0,
+        0.0, 1.0, 1.0,
+        1.0, 1.0, 1.0,
+
+        0.0, 1.0, 0.0,
+        1.0, 1.0, 1.0,
+        1.0, 1.0, 0.0,
+
+        //5
+        1.0, 0.0, 1.0,
+        1.0, 0.0, 0.0,
+        1.0, 1.0, 0.0,
+
+        1.0, 0.0, 1.0,
+        1.0, 1.0, 0.0,
+        1.0, 1.0, 1.0,
+
+        //6
+        0.0, 1.0, 1.0,
+        0.0, 1.0, 0.0,
+        0.0, 0.0, 0.0,
+
+        0.0, 1.0, 1.0,
+        0.0, 0.0, 0.0,
+        0.0, 0.0, 1.0,
     ];
+
     let vertex = Vertex::new(
         (BUF_LEN * mem::size_of::<GLfloat>()) as GLsizeiptr,
         buffer_array.as_ptr() as *const c_void,
@@ -92,6 +140,14 @@ fn main() {
     let renderer = imgui_opengl_renderer::Renderer::new(&mut imgui_context,|s|{
         video_subsystem.gl_get_proc_address(s) as _
     });
+
+    let mut depth_test:bool = true;
+    let mut blend:bool = true;
+    let mut wireframe:bool = true;
+    let mut culling:bool = true;
+    let mut camera_x:f32 = 5.0f32;
+    let mut camera_y:f32 = -5.0f32;
+    let mut camera_z:f32 = 5.0f32;
 
     let mut event_pump = sdl_context.event_pump().unwrap();
 
@@ -115,29 +171,54 @@ fn main() {
         }
 
         unsafe {
+            if depth_test {
+                gl::Enable( gl::DEPTH_TEST );
+            } else {
+                gl::Disable( gl::DEPTH_TEST );
+            }
+
+            if blend {
+                gl::Enable( gl::BLEND );
+                gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
+            } else {
+                gl::Disable( gl::BLEND );
+            }
+
+            if wireframe {
+                gl::PolygonMode(gl::FRONT_AND_BACK, gl::LINE );
+            } else {
+                gl::PolygonMode(gl::FRONT_AND_BACK, gl::FILL );
+            }
+
+            if culling {
+                gl::Enable( gl::CULL_FACE );
+            } else {
+                gl::Disable( gl::CULL_FACE );
+            }
+
             gl::Viewport(0,0,WINODW_WIDTH as i32, WINODW_HEIGHT as i32);
 
             //clear screen
             gl::ClearColor(1.0,1.0,1.0,1.0);
-            gl::Clear(gl::COLOR_BUFFER_BIT);
+            gl::Clear( gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT );
 
             //init matrice for model, view and projection
             let model_matrix = Matrix4::identity();
             let view_matrix = Matrix4::look_at(
                 Point3{
-                    x:0.0,
-                    y:0.0,
-                    z:5.0,
+                    x:camera_x,
+                    y:camera_y,
+                    z:camera_z,
                 },
                 Point3{
-                    x:0.0,
-                    y:0.0,
-                    z:0.0,
+                    x:0.5,
+                    y:0.5,
+                    z:0.5,
                 },
                 Vector3{
                     x:0.0,
-                    y:1.0,
-                    z:0.0,
+                    y:0.0,
+                    z:1.0,
                 },
             );
             let projection_matrix:Matrix4 = perspective(
@@ -154,6 +235,7 @@ fn main() {
             shader.set_mat4(c_str!("uProjection"), &projection_matrix);
 
             vertex.draw();
+
             //SDL2における画面サイズやマウス、キーボードの状態などをImguiと調整する
             imgui_sdl2_context.prepare_frame(
                 imgui_context.io_mut(),
@@ -164,8 +246,8 @@ fn main() {
             //ウィジェットの生成を助けるUI構造体を生成する
             let ui = imgui_context.frame();
             imgui::Window::new(im_str!("Imformation"))
-                .size([300.0,200.0],imgui::Condition::FirstUseEver)
-                .build(&ui,||{
+                .size([300.0,300.0], imgui::Condition::FirstUseEver)
+                .build(&ui, || {
                     ui.text(im_str!("OpenGL Test App Ver 1.0"));
                     ui.separator();
                     ui.text(im_str!("FPS: {:.1}",ui.io().framerate));
@@ -179,6 +261,19 @@ fn main() {
                         "Mouse Position: ({:.1},{:.1})",
                         mouse_pos[0],mouse_pos[1]
                     ));
+
+                    ui.separator();
+
+                    ui.checkbox(im_str!("Depth Test"), &mut depth_test);
+                    ui.checkbox(im_str!("Blend"), &mut blend);
+                    ui.checkbox(im_str!("Wireframe"), &mut wireframe);
+                    ui.checkbox(im_str!("Culling"), &mut culling);
+
+                    ui.separator();
+
+                    imgui::Slider::new(im_str!("Camera X"), -5.0..=5.0).build(&ui, &mut camera_x);
+                    imgui::Slider::new(im_str!("Camera Y"), -5.0..=5.0).build(&ui, &mut camera_y);
+                    imgui::Slider::new(im_str!("Camera Z"), -5.0..=5.0).build(&ui, &mut camera_z);
                 });
 
             //SDL2とImGuiの間でマウスの位置やマウスのカーソルの情報を調整する
